2941307dc99276199dd875b938f560dd89ea609a
seo-sang-ho <rocter2049@naver.com>
2026-06-23 15:38:53 +0900

    Initial commit

---
+++ backend/src/main/java/com/trip/whereU/WhereUApplication.java
@@ -0,0 +1,13 @@
+package com.trip.whereU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereUApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/trip/whereU/WhereUApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trip.whereU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
